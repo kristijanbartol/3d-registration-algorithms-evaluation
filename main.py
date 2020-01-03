dabdad0f7e29706cc614f69cfed6d6e0f5e29d79
Kristijan Bartol <kristijan.bartol@gmail.com>
2020-01-03 18:11:10 +0100

    Save all results.

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ ITEMS = ['bunny', 'horse', 'hand', 'dragon', 'happy']#, 'blade']
 
 VOXEL_SIZE = 0.05   # means 5cm for the dataset
 
+result_dict = {}
+
 
 def draw_registration_result(source, target, transformation=None):
     source_temp = copy.deepcopy(source)
@@ -24,67 +26,55 @@ def draw_registration_result(source, target, transformation=None):
 
 
 def save_results(source, target, result, data_root, object_name, algo):
-    #pc_path = join(data_root, '{}_{}.ply'.format(object_name, algo))
-    #print('Saving resulting point cloud...')
-    #o3d.io.write_point_cloud(pc_path, source, write_ascii=True)
-
     metrics_path = join(data_root, '{}_{}.json'.format(object_name, algo))
     metrics = { 
                 'rmse': result.inlier_rmse, 
                 'fitness': result.fitness
               }
-    print('Saving metrics...')
     with open(metrics_path, 'w') as mp:
         json.dump(metrics, mp)
+    result_dict['{}_{}'.format(object_name, algo)] = metrics
 
 
-def preprocess_point_cloud(pcd, voxel_size):
+def preprocess_point_cloud(pcd, voxel_size, object_name):
     start_time = time.time()
     pcd_down = pcd.voxel_down_sample(voxel_size)
-    print(":: Downsample with a voxel size {:.3f} ({:.2f}s).".format(
-        voxel_size, time.time() - start_time))
 
     radius_normal = voxel_size * 2
     start_time = time.time()
     pcd_down.estimate_normals(
         o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
-    print(":: Estimate normal with search radius {:.3f} ({:.2f}s).".format(
-        radius_normal, time.time() - start_time))
 
     radius_feature = voxel_size * 5
     start_time = time.time()
     pcd_fpfh = o3d.registration.compute_fpfh_feature(
         pcd_down,
         o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
-    print(":: Compute FPFH feature with search radius {:.3f} ({:.2f}s).".format(
-        radius_feature, time.time() - start_time))
+    print('[{}] Compute FPFH feature with search radius {:.3f} ({:.2f}s).'.format(
+        object_name, radius_feature, time.time() - start_time))
     return pcd_down, pcd_fpfh
 
 
 def prepare_dataset(voxel_size, data_root, object_name):
-    print(":: Load two point clouds and disturb initial pose.")
     source_path = join(data_root, '{}.{}'.format(object_name, 'ply'))
     target_path = join(data_root, '{}_trans.{}'.format(object_name, 'ply'))
     source = o3d.io.read_point_cloud(source_path)
     #target = source.translate(np.array([0.1, 0.1, 0.1])).rotate(np.array([0.5, 0.5, 0.5]))
     #o3d.io.write_point_cloud(target_path, target, write_ascii=True)
     target = o3d.io.read_point_cloud(target_path)
-    trans_init = np.asarray([[0.0, 0.0, 1.0, 0.0], [1.0, 0.0, 0.0, 0.0],
-                             [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 0.0, 1.0]])
+    #trans_init = np.asarray([[0.0, 0.0, 1.0, 0.0], [1.0, 0.0, 0.0, 0.0],
+    #                         [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 0.0, 1.0]])
     #source.transform(trans_init)
     draw_registration_result(source, target, np.identity(4))
 
-    source_down, source_fpfh = preprocess_point_cloud(source, voxel_size)
-    target_down, target_fpfh = preprocess_point_cloud(target, voxel_size)
+    source_down, source_fpfh = preprocess_point_cloud(source, voxel_size, object_name)
+    target_down, target_fpfh = preprocess_point_cloud(target, voxel_size, object_name)
     return source, target, source_down, target_down, source_fpfh, target_fpfh
 
 
 def execute_global_registration(source_down, target_down, source_fpfh,
                                 target_fpfh, voxel_size):
     distance_threshold = voxel_size * 1.5
-    print(":: RANSAC registration on downsampled point clouds.")
-    print("   Since the downsampling voxel size is %.3f," % voxel_size)
-    print("   we use a liberal distance threshold %.3f." % distance_threshold)
     result = o3d.registration.registration_ransac_based_on_feature_matching(
         source_down, target_down, source_fpfh, target_fpfh, distance_threshold,
         o3d.registration.TransformationEstimationPointToPoint(False), 4, [
@@ -98,8 +88,6 @@ def execute_global_registration(source_down, target_down, source_fpfh,
 def execute_fast_global_registration(source_down, target_down, source_fpfh,
                                      target_fpfh, voxel_size):
     distance_threshold = voxel_size * 0.5
-    print(":: Apply fast global registration with distance threshold %.3f" \
-            % distance_threshold)
     result = o3d.registration.registration_fast_based_on_feature_matching(
         source_down, target_down, source_fpfh, target_fpfh,
         o3d.registration.FastGlobalRegistrationOption(
@@ -109,14 +97,10 @@ def execute_fast_global_registration(source_down, target_down, source_fpfh,
 
 def refine_registration(source, target, source_fpfh, target_fpfh, voxel_size, result_ransac, icp_type):
     distance_threshold = voxel_size * 0.4
-    print(":: Point-to-plane ICP registration is applied on original point")
-    print("   clouds to refine the alignment. This time we use a strict")
-    print("   distance threshold %.3f." % distance_threshold)
     result = o3d.registration.registration_icp(
         source, target, distance_threshold, result_ransac.transformation,
         o3d.registration.TransformationEstimationPointToPoint() if icp_type == 'point' \
-            else o3d.registration.TransformationEstimationPointToPlane())#,
-        #criteria=o3d.registration.ICPConvergenceCriteria(relative_fitness=0.1))
+            else o3d.registration.TransformationEstimationPointToPlane())
     return result
 
 
@@ -128,8 +112,7 @@ def coarse_fine_matching(name, icp_type):
     result_ransac = execute_global_registration(source_down, target_down,
                                                 source_fpfh, target_fpfh,
                                                 VOXEL_SIZE)
-    print('Result RANSAC: {} ({:.2f}s)'.format(result_ransac, time.time() - start_time))
-    print(result_ransac.transformation)
+    print('[{}] Result RANSAC: {} ({:.2f}s)'.format(name, result_ransac, time.time() - start_time))
     draw_registration_result(source, target,
                             result_ransac.transformation)
     save_results(source, target, result_ransac, DATA_DIR, name, 'fpfh-ransac')
@@ -137,8 +120,7 @@ def coarse_fine_matching(name, icp_type):
     start_time = time.time()
     result_icp = refine_registration(source_down, target_down, source_fpfh, target_fpfh,
                                     VOXEL_SIZE, result_ransac, icp_type)
-    print('Result ICP: {} ({:.2f}s)'.format(result_icp, time.time() - start_time))
-    print(result_icp.transformation)
+    print('[{}] Result ICP: {} ({:.2f}s)'.format(name, result_icp, time.time() - start_time))
     draw_registration_result(source, target, result_icp.transformation)
     
     save_results(source, target, result_icp, DATA_DIR, name, 'fpfh-ransac-{}-icp'.format(icp_type))
@@ -152,7 +134,9 @@ def fast_global_matching(name, icp_type):
     result_fast = execute_fast_global_registration(source_down, target_down,
                                                    source_fpfh, target_fpfh,
                                                    VOXEL_SIZE)
-    print('Result FGR: {} ({:.2f}s)'.format(result_fast, time.time() - start_time))
+    result_fast = o3d.registration.evaluate_registration(
+        source, target, 0.01, transformation=result_fast.transformation)                                     
+    print('[{}] Result FGR: {} ({:.2f}s)'.format(name, result_fast, time.time() - start_time))
     draw_registration_result(source, target,
                              result_fast.transformation)
     save_results(source, target, result_fast, DATA_DIR, name, 'fgr')
@@ -160,7 +144,7 @@ def fast_global_matching(name, icp_type):
     start_time = time.time()
     result_icp = refine_registration(source_down, target_down, source_fpfh, target_fpfh,
                                     VOXEL_SIZE, result_fast, icp_type)
-    print('Result ICP: {} ({:.2f}s)'.format(result_icp, time.time() - start_time))
+    print('[{}] Result ICP: {} ({:.2f}s)'.format(name, result_icp, time.time() - start_time))
     draw_registration_result(source, target, result_icp.transformation)
     save_results(source, target, result_icp, DATA_DIR, name, 'fgr-{}-icp'.format(icp_type))
 
@@ -203,7 +187,7 @@ def go_icp(name):
     start_time = time.time()
     goicp.BuildDT()
     goicp.Register()
-    print('Go-ICP time: {:.2f}'.format(time.time() - start_time))
+    goicp_time = time.time() - start_time
 
     source = goicp_to_o3d(src_points)
     target = goicp_to_o3d(tgt_points)
@@ -214,11 +198,21 @@ def go_icp(name):
         goicp.optimalTranslation())
     draw_registration_result(target, source, goicp_transformation)
 
-    #save_results(source, target, result_icp, DATA_DIR, name, 'fgr-icp')
+    result_goicp = o3d.registration.evaluate_registration(
+        target, source, 0.001, transformation=goicp_transformation)
+    print(goicp_transformation) 
+    print('[{}] Result Go-ICP: {} ({:.2f})'.format(name, result_goicp, goicp_time))
+
+    save_results(source, target, result_goicp, DATA_DIR, name, 'goicp')
 
 
 if __name__ == "__main__":
     for name in ITEMS:
         coarse_fine_matching(name, 'point')
-        #fast_global_matching(name, 'plane')
-        #go_icp(name)
+        coarse_fine_matching(name, 'plane')
+        fast_global_matching(name, 'point')
+        fast_global_matching(name, 'plane')
+        go_icp(name)
+
+    with open(join(DATA_DIR, 'results.json'), 'w') as fr:
+        json.dump(result_dict, fr)
